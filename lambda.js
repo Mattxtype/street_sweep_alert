3c76c20df81e5303ec698165d94d50fdf6457727
Matthew Rose <matthewrose@Matthews-MacBook-Air.local>
2024-08-01 13:32:01 -0700

    add packages to docker file

--- lambda.js
+++ lambda.js
@@ -3,6 +3,7 @@ const { publish } = require("./actions/publish-topic");
 exports.handler = async function(event) {
     console.log("request:", JSON.stringify(event, undefined, 2));
     const dayOfMonth = new Date().getDate();
+    console.log(dayOfMonth);
 
     // if((dayOfMonth + 1) === 2 || (dayOfMonth + 1) === 4) {
       publish();
