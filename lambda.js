a8a3aabe23b5783a947d2220e93dcfa277915577
Matthew Rose <matthewrose@Matthews-MacBook-Air.local>
2024-08-01 14:50:09 -0700

    another test

--- lambda.js
+++ lambda.js
@@ -4,6 +4,7 @@ export async function handler(event) {
     console.log("request:", JSON.stringify(event, undefined, 2));
     const dayOfMonth = new Date().getDate();
     console.log(dayOfMonth);
+    //
 
     // if((dayOfMonth + 1) === 2 || (dayOfMonth + 1) === 4) {
       publish();
